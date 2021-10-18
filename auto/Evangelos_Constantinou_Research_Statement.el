7743eefee169d4a2614af0f9ca293f09b33ec8a7
Evangelos Constantinou <vangelas87@googlemail.com>
2021-10-17 21:21:36 -0500

    first commit

---
+++ auto/Evangelos_Constantinou_Research_Statement.el
@@ -0,0 +1,53 @@
+(TeX-add-style-hook
+ "Evangelos_Constantinou_Research_Statement"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("ulem" "normalem") ("inputenc" "utf8" "latin9") ("footmisc" "bottom") ("hyperref" "unicode=true" "pdfusetitle" "bookmarks=true" "bookmarksnumbered=false" "bookmarksopen=false" "breaklinks=false" "backref=false" "colorlinks=false")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "calc"
+    "titlesec"
+    "footmisc"
+    "multicol"
+    "subcaption"
+    "babel"
+    "esint"
+    "natbib"
+    "tabularx"
+    "booktabs"
+    "breakurl"
+    "tikz"
+    "pgfplots")
+   (LaTeX-add-labels
+    "sec:orgd4af66e"
+    "sec:org864cfad"
+    "sec:orgfb75ccb"
+    "sec:org47e9195"))
+ :latex)
+
